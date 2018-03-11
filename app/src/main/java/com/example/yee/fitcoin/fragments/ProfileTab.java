4345df0870aff414c2e7a9b077d45d86c212aaaf
Adam Yee <adam.yee@gmail.com>
2018-03-11 05:53:58 -0400

    oops i forget smthin methinks

--- app/src/main/java/com/example/yee/fitcoin/fragments/ProfileTab.java
+++ app/src/main/java/com/example/yee/fitcoin/fragments/ProfileTab.java
@@ -5,7 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.yee.fitcoin.LoginActivity;
 import com.example.yee.fitcoin.R;
 
 /**
@@ -15,8 +17,10 @@ import com.example.yee.fitcoin.R;
 public class ProfileTab extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        String userID = getActivity().getIntent().getStringExtra(LoginActivity.KEY_USER_ID);
         View view = inflater.inflate(R.layout.tab_profile, container, false);
-
+        TextView tv1 = (TextView) view.findViewById(R.id.user_text);
+        tv1.setText(userID);
         return view;
     }
 }
